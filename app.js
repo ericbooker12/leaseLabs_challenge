c50b52995b087175273d0981fc32aedc43bd742d
Eric Booker <ericbooker12@gmail.com>
2019-05-19 07:45:16 -0700

    Set up files for html, css and sass

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ var images = {
 	man: {
 		heading: 'Man Image',
 		description: 'Image of a man.',
-		path: 'img/man.jpg'
+		path: 'images/man.jpg'
 	},
 	wizard: {
 		heading: 'Wizard Image',
 		description: 'Image of a wizard.',
-		path: 'img/wizard.jpg'
+		path: 'images/wizard.jpg'
 	},
 	beast: {
 		heading: 'Beast Image',
 		description: 'Image of a beast.',
-		path: 'img/beast.jpg'
+		path: 'images/beast.jpg'
 	}
 };
+
+
+let pic1 = document.getElementById('pic1');
+let pic2 = document.getElementById('pic2');
+let pic3 = document.getElementById('pic3');
+
+window.onload = () => {
+	pic1.innerHTML = `<img src= ${images.man.path} '>`;
+	pic2.innerHTML = `<img src= ${images.wizard.path} '>`;
+	pic3.innerHTML = `<img src= ${images.beast.path} '>`;
+}
